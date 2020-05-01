cb7e7d4c0aac7063bb71a9a201c24f8e096ca6eb
ZiangXuu <ziangxuu@gmail.com>
2020-05-01 16:01:01 -0400

    added the db model and insert some data

--- django_project/blog/admin.py
+++ django_project/blog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Post #Since they are under the same directory
 
-# Register your models here.
+#Note: regirster the Post model to admin site
+admin.site.register(Post)
--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -5,5 +5,12 @@ from django.contrib.auth.models import User
 class Post(models.Model):
 	title = models.CharField(max_length=100)
 	content = models.TextField()
+	#Note:Notice that we didn't put the parentheses after timezone.now 
+	#  since we don't actually want to execute that function at that point, we just want to pass in the actual function as default value
 	date_posted = models.DateTimeField(default = timezone.now)
+	#Note: User created post and then the user was deleted then do we want to delete the post
 	author = models.ForeignKey(User, on_delete = models.CASCADE)
+
+	def __str__(self):  #Note: Represent the object by its title
+		return self.title
+
